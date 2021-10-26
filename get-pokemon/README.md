36767f2639ec72d1fc4883297b577369ee272c52
Keven Alves <kevenalvesm@gmail.com>
2021-10-26 12:20:04 -0300

    projeto getPokemon finalizado

---
+++ get-pokemon/README.md
@@ -0,0 +1,43 @@
+# GetPokemon - Live DIO - Trabalhando com classes em Javascript
+
+Esse repositório foi criado para o projeto da Live do Bootcamp Impulso React Web Developer da Digital Innovation One. <br />\
+Veja o projeto GetPokemon de forma interativa - [Visite](https://get-pokemon-kev.netlify.app/)
+
+<p align="center">
+  <img src="./assets/img/readme-bootcamp-image.png" width="188" height="400"/>
+</p>
+
+---
+
+## 📚 Assuntos tratados na Live
+
+* Definição de classe
+* Instanciando uma classe
+* Métodos `set` e `get`
+* Hosting
+* Modo Strict
+* Propriedades e Métodos Static
+* Extends
+
+---
+
+## 🔧 Mudanças implementadas
+
+* Adição da propriedade Peso
+* Adição da propriedade Tipo
+* Altereção no design
+* Refatoração de CSS para SCSS
+
+---
+
+## 🖥️ preview final do projeto desktop
+
+<p align="center">
+  <img src="assets/img/mobile.png" width="895" height="740"/>
+</p>
+
+## 📳 preview final do projeto mobile
+
+<p align="center">
+  <img src="assets/img/mobile.png" width="375" height="667"/>
+</p>
---
+++ get-pokemon/assets/js/pokemon.js
@@ -0,0 +1,51 @@
+class Pokemon {
+  constructor(name, abilities, height, baseExperience, sprites, weight, types) {
+    this._name = name
+    this._height = height
+    this._abilities = abilities
+    this._baseExperience = baseExperience
+    this._sprites = sprites
+    this._weight = weight
+    this._types = types
+  }
+ 
+  get name() {
+    return this._capitalize(this._name)
+  }
+
+  get abilities() {
+    return this._abilitiesFilter(this._abilities)
+  }
+
+  get height() {
+    return `${this._height}m` 
+  }
+
+  get baseExperience() {
+    return `${this._baseExperience}/100` 
+  }
+
+  get weight(){
+    return `${this._weight}Kg`
+  }
+
+  get types(){
+    return this._typesFilter(this._types)
+  }
+
+  get image() {
+    return this._sprites.front_default
+  }
+
+  _capitalize(value) {
+    return `${value.charAt(0).toUpperCase()}${value.slice(1)}`
+  } 
+
+  _abilitiesFilter(abilities) {
+    return abilities.map(item => ` ${this._capitalize(item.ability.name)}`)
+  }
+
+  _typesFilter(types) {
+    return types.map(item => ` ${this._capitalize(item.type.name)}`)
+  }
+}
