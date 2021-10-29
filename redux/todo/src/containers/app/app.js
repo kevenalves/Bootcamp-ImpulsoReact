b18e136c1cfa6749159ec469c135f1829d78c7ee
Keven Alves <kevenalvesm@gmail.com>
2021-10-29 12:11:02 -0300

    redux gerenciamento de estados das aplicacoes

---
+++ redux/todo/src/containers/app/app.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import Button from '../../components/button';
+import Input from '../../components/input';
+import List from '../../components/list';
+
+import {addTodo} from '../../store/actions/todo';
+
+class App extends Component {
+    state = {
+        input: ''
+    };
+
+    handleOnClick = () => {
+        const { addTodo } = this.props;
+        const { input } = this.state;
+
+        addTodo(input);
+    }
+
+    handleOnChange = (event) => {
+        this.setState({input: event.target.value });
+    }
+
+    render(){
+        const { input } = this.state;
+        const { listTodo } = this.props;
+        return (
+            <div>
+                <List />
+                <Input onChange={event => this.handleOnChange(event)} value={input} />
+                <Button
+                onClick={() => this.handleOnClick}> Adicionar </Button>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    listTodo: state.todo
+});
+
+
+export default connect(
+    mapStateToProps,
+    {addTodo}
+)(App);
